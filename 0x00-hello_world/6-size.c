96483889633e04a8a8919c8f0642c7222d2dd8f6
Valentin Vandi <vandivalentine@gmail.com>
2023-06-16 12:53:18 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,4 +7,11 @@
  */
 int main(void)
 {
-	
+	printf("%lu size of a\n", sizeof(char));
+	printf("%lu size of a\n", sizeof(int));
+	printf("%lu size of a\n", sizeof(long int));
+	printf("%lu size of a\n", sizeof(long long));
+	printf("%lu size of a\n", sizeof(float));
+
+	return (0);
+}
