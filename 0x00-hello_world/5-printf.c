4b112ac8d810f6707e0cce1d6c2e9d3d9b9cb538
Valentin Vandi <vandivalentine@gmail.com>
2023-06-16 10:12:12 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-/*main - Entry point of program.*/
-/*return: Always 0 (Success).*/
+/** 
+ * main - Entry point of program.
+ * 
+ * return: Always 0 (Success).
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
