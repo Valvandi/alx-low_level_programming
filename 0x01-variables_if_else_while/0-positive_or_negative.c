f1ffab6377061b2b69074ff1914b4dc16b22f14f
Valentin Vandi <vandivalentine@gmail.com>
2023-06-18 16:25:14 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/* main -Entry point of the program*/
+/* main function: Entry point of the program*/
 int main(void)
 {
 	int n;
