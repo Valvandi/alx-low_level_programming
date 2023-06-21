4e2ef7ffa3c4553179fa955aafb8285ee078932d
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 03:41:38 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
- * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
+		_putchar(c);
 	}
+	_putchar('\n');
 }
