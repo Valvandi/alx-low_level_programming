8c0a15eeb5b8e8d8bbf84729f73b2aedbf8ba6aa
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 04:28:58 -0700

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower(int c): check the code
+ *
+ */
+
+int _islower (int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+
+	}
+
+
+
+}
