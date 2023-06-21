409187d2ec22dc990487e3536d25608cde13e511
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 14:23:37 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
  *Return: returns 0 (Success)
  */
-int main(void){
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Introduction to low-level C programming 
