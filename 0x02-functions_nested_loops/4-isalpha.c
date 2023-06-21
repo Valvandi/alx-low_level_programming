0f313efe6f1055c42a8ad4d2c2b6c57ea6caf849
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 11:59:58 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,17 @@
-#include main.h
+#include "main.h"
+/**
+ * _isalpha - checks if character is letter, lowercase or uppercase
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase else return 0
+ *
+ * @c: variable asignment
+ *
+ */
 int _isalpha(int c)
 {
 	char c;
 
-	if (c == 'a' && c = 'a' || c = 'A')
+	if (c >= 'a' && c <= 'z') || (c <= 'A' && c >= 'Z')
 	{
 		return (1);
 	}
