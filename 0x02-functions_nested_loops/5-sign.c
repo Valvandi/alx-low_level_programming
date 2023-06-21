a68ab387004d6c3af1d9ab43cdf7ffcbc0ccb19c
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 13:38:56 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,25 @@
 
 int print_sign(int n)
 {
+	int value;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		value = 1;
 	}
 	else if (n = 0)
 	{
 		_putchar('0');
-		return 0;
+		value = 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		value = -1;
+	}
+
+	return value;
 
 }
 
