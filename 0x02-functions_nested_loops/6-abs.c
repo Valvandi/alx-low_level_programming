ed9edacf52622e4fe549e34b3d7cc5bbf4e4ed1f
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 13:52:48 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - checks the code
+ *
+ * Return: always 0
+ *
+ * @n: A variable
+ */
+
+int _abs(int)
+{
+	int n;
+	n = abs(n);
+	_putchar(n);
+
+	return (0)
+}
+
+
+
