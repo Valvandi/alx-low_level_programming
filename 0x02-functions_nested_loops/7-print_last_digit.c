bb3f55ac88d8c26cab09478123eb0834ec7d3482
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 14:42:31 -0700

    printing last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,13 @@
 /**
  * print_last_digit - checks the code
  *
- * Return: always 0 
+ * Return: always 0
  *
  * @n: variable
  */
 
 int print_last_digit(int n)
-{ 
+{
 
 	n  = n % 10;
 	return (n);
