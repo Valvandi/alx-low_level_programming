8a4ad5b99a740a21787c033aa77bf860e4e33cbb
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 04:00:53 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	while int i = 10
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+
+		}
+
+	_putchar('\n');
+
+}
