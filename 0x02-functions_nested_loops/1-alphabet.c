0a6a828e7db0c377572a3db8cab49f07f02518fe
Valentin Vandi <vandivalentine@gmail.com>
2023-06-21 03:24:10 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	void print_alphabet(void)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+}
