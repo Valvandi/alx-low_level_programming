f30217ef52372a0e8f3245df98168a899de7f33a
Valentin Vandi <vandivalentine@gmail.com>
2023-06-23 02:56:06 -0700

    checks for numbers 0 - 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -12,21 +12,14 @@
 
 int _isdigit(int c)
 {
-
-	for (c = 0; c < 9; c++)
+	if (isdigit(c))
 	{
-		if (isdigit(c))
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-
-		}
-		return (c);
+		return (1);
 	}
-
+	else
+	{
+		return (0);
+	}
 	return (c);
 
 }
