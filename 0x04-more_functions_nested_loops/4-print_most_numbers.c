05999e6fafab0f0212d42e56cf3122fd32c4335e
Valentin Vandi <vandivalentine@gmail.com>
2023-06-23 11:47:22 -0700

    prints number from 0 - 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_most_numbers - check code
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+
+	}
+}
