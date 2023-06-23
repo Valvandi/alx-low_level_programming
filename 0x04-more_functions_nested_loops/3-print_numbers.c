d58799bc21b3a7da488b8a19c68ef9b29bb71d0a
Valentin Vandi <vandivalentine@gmail.com>
2023-06-23 01:38:21 -0700

    prints number from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_numbers - checks code
+ *
+ * Return: i at the end of program
+ *
+ * @i - variable declared in function
+ *
+ */
+
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+
+	}
+	return (i);
+
+}
+
