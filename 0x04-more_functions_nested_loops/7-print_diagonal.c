f29a0a8ad83066af4a8db282458bae138f432082
Valentin Vandi <vandivalentine@gmail.com>
2023-06-23 17:09:18 -0700

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_diagonal(int n )
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	int spaces = 0;
+	while (spaces < n)
+	{
+		int i;
+		for (i = 0; i < spaces; i++)
+		{
+			putchar(' ');
+		}
+
+		putchar('\\');
+		putchat('\n');
+		spaces++;
+	}
+}
