c57c7b4acc7219a982feb498d089819f54f298fc
Valentin Vandi <vandivalentine@gmail.com>
2023-06-26 15:04:00 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen- check len of string
+ *
+ * Return: always 0
+ *
+ *
+ */
+int _strlen(char *s)
+{
+	char value = *s;
+
+	strlen(value);
+
+	return (value);
+
+}
+
+
+
