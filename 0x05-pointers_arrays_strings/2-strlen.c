6f973fb89da6a401a7ed0152e1a320987435836f
Valentin Vandi <vandivalentine@gmail.com>
2023-06-26 18:10:17 -0700

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,14 @@
 #include <string.h>
 #include <stdio.h>
-
+/**
+ *
+ *
+ *
+ *
+ *
+ */
 int _strlen(char *s) {
-    int length = 0;
-    while (*s != '\0') {
-        length++;
-        s++;
-    }
-    return length;
-}
-
-int main() {
-    char str[] = "Hello, world!";
-    int length = _strlen(str);
-    printf("Length of the string: %d\n", length);
-    return 0;
+  
+	len = (*s);
+	strlen(len);
 }
