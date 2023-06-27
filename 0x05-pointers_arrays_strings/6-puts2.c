c4e8f932747dad848a844239d5cead37bc6a62a4
Valentin Vandi <vandivalentine@gmail.com>
2023-06-27 06:34:47 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 /**
+ * puts2 - check code
  *
- *
- *
- *
- *
- *
+ * @str: function argument
  */
 void puts2(char *str)
 {
+	int i;
+	for (i = 0; i <= str; i++)
+	{
+		puts(i);
 
-
-
-
-
+	}
 
 }
