abde018b84439dc1561bb36e27b38e0727c0d703
Valentin Vandi <vandivalentine@gmail.com>
2023-06-27 16:45:23 -0700

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 /**
+ * puts_half - check code
  *
- *
- *
+ * @str: function variable
  *
  */
 void puts_half(char *str)
 {
+	int i;
+	int length = (strlen(str) - 1);
+	int half_length = ((strlen(str) - 1) / 2);
 
-
-
-
-
+	for (i = 0; i <= length; i++)
+	{
+		if (length % 2 == 0)
+		{
+			printf("%s\n", half_length);
+		}
+		else
+		{
+			printf("%s\n", str + half_length);
+		}
+	}
 }
 
