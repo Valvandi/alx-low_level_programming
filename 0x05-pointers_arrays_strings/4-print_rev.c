d6384efde49423b7a03a22eb2661966381405a8b
Valentin Vandi <vandivalentine@gmail.com>
2023-06-27 03:46:34 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 /**
+ * print_rev - function 
  *
- *
- *
- *
- *
+ * @s: parameter od function
  */
 void print_rev(char *s)
 {
-
-
+	strrev(s);
 
 }
 
