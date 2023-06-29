7ea45de5af26b0f6d1d807ab67e78645ff81755f
Valentin Vandi <vandivalentine@gmail.com>
2023-06-29 08:08:46 -0700

    new update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * @*_strcat - function to concatenate strings
+ *
+ * @dest: function argument
+ *
+ * @src: function argument
+ *
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(desr, src);
+
+	return (0);
+}
+
+
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a continuation of C pointers, arrays and strings manipulation
