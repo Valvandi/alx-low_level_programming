4d312d0d2c8fa5aa130ee9bafd3c22866f128b20
Valentin Vandi <vandivalentine@gmail.com>
2023-06-29 15:04:15 -0700

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * @*_strncat - function to concat strings
+ *
+ * @dest: function argument
+ *
+ * @src: function argument
+ *
+ * @n: function argument
+ *
+ * Return: returns value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char str_len = strlen(dest);
+
+	strcat(dest + str_len, src, n);
+
+	return (dest);
+
+}
