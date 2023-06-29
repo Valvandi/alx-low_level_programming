c2ace7873cb3e393a13af692afb9a0c08ce8a9e4
Valentin Vandi <vandivalentine@gmail.com>
2023-06-29 15:54:00 -0700

    update *

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * void reverse_array- function to reverse string
+ *
+ * @a: parameter
+ *
+ * @n: parameter
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j -1));
+			*(a + (j - 1)) = temp;
+		}
+	}
+
+
+
+}
+
