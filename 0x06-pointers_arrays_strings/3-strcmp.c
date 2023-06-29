9879f03594386fed133ad4fece173dd7c86418a3
Valentin Vandi <vandivalentine@gmail.com>
2023-06-29 15:45:22 -0700

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - function to compare strings
+ *
+ * @s1: function parameter
+ *
+ * @s2: function parameter
+ *
+ * Return: returns value specified
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1,s2);
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 void _puts(char *str);
+int _strcmp(char *s1, char *s2);
