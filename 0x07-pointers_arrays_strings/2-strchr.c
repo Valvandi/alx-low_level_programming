e4918e9171a19d8d5c6af263fc0fb41558b22192
Valentin Vandi <vandivalentine@gmail.com>
2023-07-03 13:47:05 -0700

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * @*_strchr - check code
+ *
+ * @s: function parameter
+ *
+ * @c: function parameter
+ *
+ * Return: always return expected value
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+
+}
+
