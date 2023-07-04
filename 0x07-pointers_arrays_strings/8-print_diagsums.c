4d0cf5bea4e4f24b4cddb64890139d8740e9286f
Valentin Vandi <vandivalentine@gmail.com>
2023-07-04 13:30:05 -0700

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,17 +2,27 @@
 #include "main.h"
 #include <string.h>
 /**
+ * print_diagsums - function to check code
  *
+ * a: function param 
  *
- *
- *
- *
- *
- *
+ * size: function param
  *
  */
 void print_diagsums(int *a, int size)
 {
+	int sum_1;
+	int sum_2;
+
+	for (int i = 0; i < size; i++)
+	{
+		sum_1 += 
+	}
+
+	for (int j = 0; j < size; j++)
+	{
+
+	}
 
 
 }
