323853c369ef9b9f1b7341fc37e4d59193efed1b
Valentin Vandi <vandivalentine@gmail.com>
2023-07-04 13:51:17 -0700

    update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * set_string - function to set string pointer
+ *
+ * @s: function param
+ *
+ * @to: function param
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
