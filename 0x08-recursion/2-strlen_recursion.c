05035e79f39717e039c745387b7e72def80c1cc6
Valentin Vandi <vandivalentine@gmail.com>
2023-07-05 07:56:31 -0700

    return string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - function to check code
+ *
+ * @s: function parameter
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
