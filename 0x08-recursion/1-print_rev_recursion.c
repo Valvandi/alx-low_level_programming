f392c905ec8993ec5913bdc9851a7946646af13c
Valentin Vandi <vandivalentine@gmail.com>
2023-07-05 07:49:39 -0700

    print a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,18 +6,11 @@
  *
  * @s: function variable
  *
- * length: parameter
- *
- * i: pramater
  */
 void _print_rev_recursion(char *s)
 {
 	int length = strlen(s);
 
 	for (int i = length - 1; i > 0; i--)
-	{
 		putchar(s[i]);
-	}
-	putchar('\n');
-
 }
