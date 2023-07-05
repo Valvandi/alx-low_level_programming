2f1f8c2fcb4bc98eedb3a0bf706f5c5f90e4eb9f
Valentin Vandi <vandivalentine@gmail.com>
2023-07-05 09:00:12 -0700

    return factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * factorial- function to check code
+ *
+ * @n: function param
+ *
+ * Return: return desired output
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * (n - 1));
+
+	}
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,2 +1,8 @@
 void _puts_recursion(char *s);
 int _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n );
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_primt_number(int n);
+
