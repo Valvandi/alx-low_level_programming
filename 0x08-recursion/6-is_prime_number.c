68f1b0afdc84831021068f5744d49467ac898007
Valentin Vandi <vandivalentine@gmail.com>
2023-07-05 15:23:22 -0700

    update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * is_prime_number - function to check code
+ *
+ * @n: function parameter
+ *
+ * Return: returns desirable output
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+}
