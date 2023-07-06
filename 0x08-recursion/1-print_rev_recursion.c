44877eeaa0eee51521649e6efe470bde69ed68e9
Valentin Vandi <vandivalentine@gmail.com>
2023-07-05 22:01:20 -0700

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,7 +9,15 @@
  */
 void _print_rev_recursion(char *s)
 {
+	int length = strlen(s);
 
+	if (length >= 1)
+	{
+		putchar(s[length -1]);
+		s[length - 1] = '\0';
+		_print_rev_recursion(s);
+		s[length - 1] = s[length];
+	}
 
 
 }
