24b45e219ee2d7ac7d489f28c23ad90142c83252
Valentin Vandi <vandivalentine@gmail.com>
2023-07-06 15:26:25 -0700

    update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,6 @@ int _strlen_recursion(char *s);
 int factorial(int n );
 int _pow_recursion(double x, double y);
 int _sqrt_recursion(int n);
-int is_primt_number(int n);
-
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
