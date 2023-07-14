c6f257a060ea2cba24086dd9dc790b97799d3dcf
Valentin Vandi <vandivalentine@gmail.com>
2023-07-14 04:41:25 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 
 	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n +1));
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
@@ -36,17 +36,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i] = s1[i];
 		i++;
 	}
-
 	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
-
 	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
 
 	return (s);
-			
 
 }
 
