fce63ebde770f113ecaba0a4fe38b4e8a017b292
Valvandi <vandivalentine@gmail.com>
2023-11-08 11:51:40 -0800

    update

--- 0x0F_function_pointers/0-print_name.c
+++ 0x0F_function_pointers/0-print_name.c
@@ -7,8 +7,33 @@
  * Return: Always 0 (Success)
 */
 
-void print_name(char *name, void (*f)(char *))
-{
-	if (name && f)
-		f(name);
+
+#include <stdio.h>
+
+void print_name(char *name, void (*f)(char *)) {
+    f(name);
 }
+
+
+void print_normal(char *name) {
+    printf("Name: %s\n", name);
+}
+
+
+void print_reverse(char *name) {
+    printf("Reversed Name: ");
+    for (int i = strlen(name) - 1; i >= 0; i--) {
+        putchar(name[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    char myName[] = "John Doe";
+
+    print_name(myName, print_normal);
+    print_name(myName, print_reverse);
+
+    return 0;
+}
+
